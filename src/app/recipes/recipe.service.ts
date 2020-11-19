a07a02098efdf2ce5e5db3098455d7070afd2d52
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-19 16:30:20 +0530

    Created new recipe service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test description',
+      'https://images.pexels.com/photos/5200286/pexels-photo-5200286.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ),
+    new Recipe(
+      'A test2 recipe',
+      'This is another test description',
+      'https://images.pexels.com/photos/5200286/pexels-photo-5200286.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ),
+  ];
+
+  getRecipes() {
+    // This may allow outside components to change this array. So we will send a copy instead
+    // return this.recipes;
+    return this.recipes.slice();
+  }
+
+  constructor() {}
+}
