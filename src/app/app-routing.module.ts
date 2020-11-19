90a6b950de6563039f254dfab510b4ef33cd71f9
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-20 02:35:41 +0530

    Created new RecipeStart component, and added default child route to recipes route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 
 const routes: Routes = [
   // Redirecting initial page to recipes page, pathMath:full means when entire path is empty
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [{ path: '', component: RecipeStartComponent }],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
