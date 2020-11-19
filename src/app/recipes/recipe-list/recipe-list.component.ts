70b6a01af9aa6c3b86795cee193fae13ef338e6d
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-20 03:58:07 +0530

    Updated navigation to new-recipe in edit child route, with programmatic approach

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,9 +11,20 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  // This is programmatic way or do this directly with routerLink="new"
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+    // Since this is relative path, we need to tell the router about the current path: so inject ActivatedRoute.
+    // Inorder to set navigate to desired path, inject Router
   }
 }
