703d865858b08027485979455aa8deaeb26fe2a1
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-21 21:04:38 +0530

    Updated existing shopping list with template driven forms

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,20 +10,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) newIngName: ElementRef;
-  @ViewChild('amountInput', { static: false }) newIngAmount: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
-  // Or you could pass local references as args to this method instead of using @ViewChild
-  onAddIngredient(e: Event) {
-    const ingName = this.newIngName.nativeElement.value;
-    const ingAmount = this.newIngAmount.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddIngredient(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
 
     this.shoppingListService.addIngredient(newIngredient);
-    e.preventDefault();
   }
 }
