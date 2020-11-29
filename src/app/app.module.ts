e712276495624b35f45e104844ef81e721668af2
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-30 00:10:59 +0530

    Revert "Setting up NgRx store for shoppingList feature in app module"
    This reverts commit 082b9e886be73781a44b49b2e7c70680f7628066.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -10,8 +9,6 @@ import { HeaderComponent } from './header/header.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -22,8 +19,6 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
 
     SharedModule,
     CoreModule,
-
-    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
   ],
   bootstrap: [AppComponent],
 })
