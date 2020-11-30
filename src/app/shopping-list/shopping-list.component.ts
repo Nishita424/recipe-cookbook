6b0516c148d3bd3b8897322d872ba1003bcbe9c5
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-30 13:48:22 +0530

    Updated getting ingredients and subscribing to changes from shoppinglist service with call to ngrx store after registering shopping list reducer to storemodule, also fetched ingredients from observable returned as an array with async pipe

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,31 +11,37 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private ingChangeSub: Subscription;
-  private ingsChangeSub: Subscription;
-  private ingsReducedSub: Subscription;
+  // ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
+  // private ingChangeSub: Subscription;
+  // private ingsChangeSub: Subscription;
+  // private ingsReducedSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
+    this.ingredients = this.store.select('shoppingList');
 
-    this.ingChangeSub = this.shoppingListService.newIngredientAdded.subscribe(
-      (ingredient: Ingredient) => {
-        this.ingredients.push(ingredient);
-      }
-    );
-    this.ingsChangeSub = this.shoppingListService.newIngredientsAdded.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
-    this.ingsReducedSub = this.shoppingListService.ingsAfterDeletion.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    // this.ingredients = this.shoppingListService.getIngredients();
+
+    // this.ingChangeSub = this.shoppingListService.newIngredientAdded.subscribe(
+    //   (ingredient: Ingredient) => {
+    //     this.ingredients.push(ingredient);
+    //   }
+    // );
+    // this.ingsChangeSub = this.shoppingListService.newIngredientsAdded.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
+    // this.ingsReducedSub = this.shoppingListService.ingsAfterDeletion.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
   }
 
   onEditIngredient(index: number) {
@@ -41,8 +49,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.ingChangeSub.unsubscribe();
-    this.ingsChangeSub.unsubscribe();
-    this.ingsReducedSub.unsubscribe();
+    // this.ingChangeSub.unsubscribe();
+    // this.ingsChangeSub.unsubscribe();
+    // this.ingsReducedSub.unsubscribe();
   }
 }
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -23,5 +23,7 @@ export function shoppingListReducer(
         ...state,
         ingredients: [...state.ingredients, action.payload],
       };
+    default:
+      return state;
   }
 }
