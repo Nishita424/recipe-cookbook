90d975938c3aeb36fbee809a9c9e2bf70af05166
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-30 14:47:25 +0530

    Added UPDATE_INGREDIENT, DELETE_INGREDIENT actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // For type safety: This must never be changed from outside
@@ -14,4 +16,18 @@ export class AddIngredients implements Action {
   constructor(public payload: Ingredient[]) {}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENT;
+  constructor(public payload: { ingredient: Ingredient; index: number }) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+  constructor(public payload: { index: number }) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredient
+  | AddIngredients
+  | UpdateIngredient
+  | DeleteIngredient;
